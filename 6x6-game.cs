7061368aa25385894377be5d41fc96197684be4f
TaifBinEid <82478321+TaifBinEid@users.noreply.github.com>
2021-05-06 05:43:20 +0300

    Update 6x6-game.cs

--- 6x6-game.cs
+++ 6x6-game.cs
@@ -29,122 +29,122 @@
     <caption> <b>Game 6x6</b> </caption>
     <tr>
            <td>
-            <div id="1" style="background-color: #e28f83;">1</div>
+            <div id="1" style="background-color: #e28f83; font-color :black">Start here!</div>
            </td> 
            <td>
-            <div id="2">2</div>
+            <div id="2"></div>
            </td> 
            <td>
-            <div id="3">3</div>
+            <div id="3"></div>
            </td>
            <td>
-            <div id="4">4</div>
+            <div id="4"></div>
            </td>
            <td>
-            <div id="5">5</div>
+            <div id="5"></div>
            </td>
            <td>
-            <div id="6">6</div>
+            <div id="6"></div>
            </td>
     </tr>
     <tr>
         <td>
-         <div id="7">7</div>
+         <div id="7"></div>
         </td> 
         <td>
-         <div id="8" >8</div>
+         <div id="8" ></div>
         </td> 
         <td>
-         <div id="9">9</div>
+         <div id="9"></div>
         </td>
         <td>
-        <div id="10">10</div>
+        <div id="10"></div>
     </td>
     <td>
-     <div id="11">11</div>
+     <div id="11"></div>
     </td>
     <td>
-     <div id="12">12</div>
+     <div id="12"></div>
     </td>
  </tr>
  <tr>
     <td>
-     <div id="13">13</div>
+     <div id="13"></div>
     </td> 
     <td>
-     <div id="14" >14</div>
+     <div id="14" ></div>
     </td> 
     <td>
-     <div id="15">15</div>
+     <div id="15"></div>
     </td>
     <td>
-    <div id="16">16</div>
+    <div id="16"></div>
 </td>
 <td>
- <div id="17">17</div>
+ <div id="17"></div>
 </td>
 <td>
- <div id="18">18</div>
+ <div id="18"></div>
 </td>
 </tr>
 <tr>
     <td>
-     <div id="19">19</div>
+     <div id="19"></div>
     </td> 
     <td>
-     <div id="20">20</div>
+     <div id="20"></div>
     </td> 
     <td>
-     <div id="21">21</div>
+     <div id="21"></div>
     </td>
     <td>
-    <div id="22">22</div>
+    <div id="22"></div>
 </td>
 <td>
- <div id="23">23</div>
+ <div id="23"></div>
 </td>
 <td>
- <div id="24">24</div>
+ <div id="24"></div>
 </td>
 </tr>
 <tr>
     <td>
-     <div id="25">25</div>
+     <div id="25"></div>
     </td> 
     <td>
-     <div id="26">26</div>
+     <div id="26"></div>
     </td> 
     <td>
-     <div id="27" >27</div>
+     <div id="27" ></div>
     </td>
     <td>
-    <div id="28">28</div>
+    <div id="28"></div>
 </td>
 <td>
- <div id="29">29</div>
+ <div id="29"></div>
 </td>
 <td>
- <div id="30">30</div>
+ <div id="30"></div>
 </td>
 </tr>
 <tr>
     <td>
-     <div id="31">31</div>
+     <div id="31"></div>
     </td> 
     <td>
-     <div id="32">32</div>
+     <div id="32"></div>
     </td> 
     <td>
-     <div id="33">33</div>
+     <div id="33"></div>
     </td>
     <td>
-    <div id="34" >34</div>
+    <div id="34" ></div>
 </td>
 <td>
- <div id="35">35</div>
+ <div id="35"></div>
 </td>
 <td>
- <div id="d36" style="background-color:#81b214;" class="glow">36</div>
+ <div id="d36" style="background-color:#81b214;" class="glow">Goal </div>
 </td>
 </tr>
 
@@ -158,7 +158,7 @@ let divs = document.getElementsByTagName("div");
 let random1= Math.floor(Math.random()*4)
 
 for (let i = 0 ;i <=divs.length; i++)
-    {  let random2 = Math.floor(Math.random() *2);
+    {  let random2 = Math.floor(Math.random() *6);
         if(random1 != random2 && random1 != random2 -1 && random1 != random2 +1 && random1 != 1 && random1 != 36 && random2 != 1 && random2 != 36 ){
             divs[i].style.backgroundColor= "#595a57";
              }
@@ -179,7 +179,8 @@ for (let i = 0 ;i <=divs.length; i++)
  
       
 
-       }}
+       }
+    }
 
     
 
